d43f236e21ec8b34a257a64d7b8acb1b864e9ebc
bahareh kefayati <56130118+bkef@users.noreply.github.com>
2019-11-06 12:02:54 -0800

    Add files via upload

---
+++ hello world/JS/main.js
@@ -0,0 +1,36 @@
+//alert("hello, Im external");
+//console.log	("hi there!");
+
+
+
+//function 3 - number
+function Age(){
+		age = parseInt(prompt("How old are you?"),10)+1;
+		alert(" you will be "+age+" years next year");
+}
+
+
+
+//function 2 - number
+function Number(){
+
+let number = parseInt(prompt("gime me a number"),10);
+let total = number + 10;
+alert(total);
+
+}
+
+
+
+//Function 1 - User name
+function Username(){
+
+let name = prompt("hello, what is your name?");
+document.getElementById("demo").innerHTML = "hello" + name;
+}
+
+
+
+function Hello(){
+		alert("Hello, World");
+}
